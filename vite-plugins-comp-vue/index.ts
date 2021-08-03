4493960aaf73d06e4e075cedb84711fc1a71e863
CavinHuang <sujinw@qq.com>
2021-08-03 17:50:48 +0800

    update

--- vite-plugins-comp-vue/index.ts
+++ vite-plugins-comp-vue/index.ts
@@ -1,12 +1,16 @@
 import { Plugin } from 'vite'
 import { APP_PATH } from './config'
 import { routerPaths } from './vite-plugin-comp-runtime/siteData'
+import { createMarkdownToVueRenderFn } from './vite-plugin-md/markdownToVue'
 
 export interface ViteCompConfig {
   docDir: string
 }
 
+let hasDeadLinks = false
+
 export default (config: ViteCompConfig): Plugin => {
+  const markdownToVue = createMarkdownToVueRenderFn(root, markdown, pages)
 
   return {
     name: 'vite-comp-vue',
@@ -21,6 +25,18 @@ export default (config: ViteCompConfig): Plugin => {
         return `export default ${JSON.stringify(await routerPaths(config))}`
       }
     },
+
+    transform(code, id) {
+      if (id.endsWith('.md')) {
+        // transform .md files into vueSrc so plugin-vue can handle it
+        const { vueSrc, deadLinks } = markdownToVue(code, id)
+        if (deadLinks.length) {
+          hasDeadLinks = true
+        }
+        return vueSrc
+      }
+    },
+
     configureServer(server) {
       // serve our index.html after vite history fallback
       return () => {
