38b83657fbfbd9c620a980a034f0c98d40ed6dca
cavinHuang <943866180@qq.com>
2021-08-07 21:54:17 +0800

    完成页面组件渲染

--- demo/src/components/Test.tsx
+++ demo/src/components/Test.tsx
@@ -5,8 +5,14 @@ export default defineComponent({
   },
   setup() {
     const number = ref(2)
+    const onClickHandler = () => {
+      number.value ++
+    }
     return () => (
-      <div>aa111aaa{ number.value }</div>
+      <>
+        <button onClick={onClickHandler}>点我+1</button>
+        <div>当前数值：{ number.value }</div>
+      </>
     )
   }
 })
--- vite-plugins-comp-vue/runtime/src/theme/default/Layout.vue
+++ vite-plugins-comp-vue/runtime/src/theme/default/Layout.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Layout</h1>
+  <page-content />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Content as PageContent } from '../../components/pageContent'
+
+export default defineComponent({
+  components: {
+    PageContent
+  }
+})
+</script>
